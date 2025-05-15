85a7991cabfad8d54ca515c74a28128c2e5caeb3
Branden <espin.brand@gmail.com>
2025-05-15 14:43:27 -0400

    node integration

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // Optional: Specify output directory (defaults to 'build')
+      out: 'build'
+    }),
+    preprocess: vitePreprocess()
   }
 };
